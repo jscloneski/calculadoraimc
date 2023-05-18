e245ce9c51efd91082ba53cfc0b724d1175c1949
João Scloneski <jscloneski.dev@gmail.com>
2023-05-18 05:44:47 -0300

    on input

--- js/script.js
+++ js/script.js
@@ -6,6 +6,9 @@ const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
 
+inputWeight.oninput = () => AlertError.close()
+inputHeight.oninput = () => AlertError.close()
+
 form.onsubmit = event => {
     event.preventDefault()
 
@@ -31,3 +34,4 @@ function displayResultMessage(result) {
     Modal.message.innerText = message
     Modal.open()
 }
+
